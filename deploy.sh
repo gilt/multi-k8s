3432aadcd5ce8c80f17a705bb846ea18b741c2c9
sameer59-saks <sameer_varpe@s5a.com>
2020-07-10 11:14:58 -0400

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+docker build -t svarpe/multi-client:latest -t svarpe/multi-client:$SHA -f ./clinet/Dockerfile ./client
+docker build -t svarpe/multi-server:latest -t svarpe/multi-server:$SHA -f /.server/Dockerfile ./server
+docker build -t svarpe/multi-worker:latest -t svarpe/multi-worker:$SHA -f /.worker/Dockerfile ./worker
+
+docker push svarpe/multi-client:latest
+docker push svarpe/multi-server:latest
+docker push svarpe/multi-worker:latest
+
+docker push svarpe/multi-client:$SHA
+docker push svarpe/multi-server:$SHA
+docker push svarpe/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=svarpe/multi-server:$SHA
+kubectl set image deployments/client-deployment client=svarpe/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=svarpe/multi-worker:$SHA
+
